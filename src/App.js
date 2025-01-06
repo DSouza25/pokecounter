77413476fbfb4f97c1ce7e30c5a046cf2bb69e72
DSouza25 <souzadario25@gmail.com>
2025-01-06 16:07:15 -0400

    primeiro commit: subindo o projeto para o github

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './app.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="background">
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
