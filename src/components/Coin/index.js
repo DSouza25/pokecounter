5062deb7171dfe2edfdce014f4264452527054e0
DSouza25 <souzadario25@gmail.com>
2025-01-08 21:17:31 -0400

    creating the function for  VAstro and coin, but coin is incomplete

--- src/components/Coin/index.js
+++ src/components/Coin/index.js
@@ -1,10 +1,43 @@
+import { useState } from 'react';
 import './coin.css';
 
 export default function Coin(){
+    const[status, setStatus] = useState(1);
+    const[nrandom, setNrandom] = useState(0)
+
+    
+    function flip(){
+        let coin = document.querySelector(".coin");
+        let random = Math.floor(Math.random()*11);
+        setNrandom(random);
+        if(random%2==0){
+            if(status == 1){
+                coin.classList.remove("flipNormal");
+                coin.classList.add("flipChange");
+            }else{
+                coin.classList.remove("flipChange");
+                coin.classList.add("flipNormal");
+            }
+            setStatus(2);
+        }else{
+            if(status == 2){
+                coin.classList.remove("flipNormal");
+                coin.classList.add("flipChange");
+            }else{
+                coin.classList.remove("flipChange");
+                coin.classList.add("flipNormal");
+            }
+            setStatus(1);
+        }
+    }
+
     return(
         <div className='coinContainer'>
-            <div className='front'></div>
-            <div className='back'></div>
+            <p style={{color: 'white'}}>{status}  {nrandom}</p>
+            <div className='coin' onClick={flip}>
+                <div className='front'></div>
+                <div className='back'></div>
+            </div>
         </div>
     )
 }
--- src/components/Vastro/index.js
+++ src/components/Vastro/index.js
@@ -1,9 +1,20 @@
+import { useState } from 'react';
 import './vastro.css';
 
 export default function Vastro(){
+    const[active, setActive] = useState(true);
+
+    function changeStatus(){
+        setActive(!active);
+    }
+
+
     return(
-        <div className='astroMark'>
-            
-        </div>
+        <>
+            {active?
+            <div onClick={changeStatus} className='astroMark'></div> :
+            <div onClick={changeStatus} className='disable'></div>
+            }
+        </>
     )
 }
